58768b24277f73bbb0ca1a5ae9defb6d90cbe176
DanTurco <danny.turco@kbx.global>
2022-02-12 20:33:41 -0600

    Bug fix nullable DateTime Updated pluralizer rules

---
+++ TestProject/Sample/Sample/protoTester.cs
@@ -0,0 +1,36 @@
+﻿using ProtoBuf;
+namespace Sample;
+
+[ProtoContract]
+public class protoTester
+{
+    [ProtoMember(1)]
+    public int MyProperty { get; set; }
+
+    [ProtoMember(2)]
+    public int MyProperty1 { get; set; }
+
+    public DateTime? NullDateTime { get; set; }
+
+    public int? NullInt { get; set; }
+
+    public int?[] NullIntArray { get; set; }
+
+    public bool CanExecute() { return true; }
+
+    public bool ShouldHappen() { return true; }
+}
+
+namespace Sample.Other
+{
+
+    [ProtoContract]
+    public class protoTester
+    {
+        [ProtoMember(1)]
+        public int MyProperty { get; set; }
+
+        [ProtoMember(2)]
+        public int MyProperty1 { get; set; }
+    }
+}
